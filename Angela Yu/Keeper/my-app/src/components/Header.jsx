31ee27a8488f0770c3e3c47abf00c2631b31bc0d
Guriya124 <guriyakumarisgvu@gmail.com>
2024-02-01 22:36:36 +0530

    Create component floder

---
+++ Angela Yu/Keeper/my-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Header() {
+    return <Header>
+        <h1>Keeper</h1>
+    </Header>
+
+
+}
+
+export default Header;
--- Angela Yu/Keeper/my-app/src/index.js
+++ Angela Yu/Keeper/my-app/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App.jsx";
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
