399f1ae0abf5c58e793056c6448c7793287bf718
Guriya124 <guriyakumarisgvu@gmail.com>
2024-02-16 23:04:03 +0530

    Complete Keeper project

--- Angela Yu/Keeper/my-app/src/components/App.jsx
+++ Angela Yu/Keeper/my-app/src/components/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
+
 
 
 export default function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(notesItem => (
+                <Note
+                    key={notesItem.key}
+                    title={notesItem.title}
+                    content={notesItem.content}
+                />
+            ))}
             <Footer />
 
         </div>
