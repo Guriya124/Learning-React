7bb28fde6650aced525bc7a78d1c6b00cf7761dd
Guriya124 <guriyakumarisgvu@gmail.com>
2024-02-22 00:05:54 +0530

    Create a Password generator app in React

---
+++ Angela Yu/my-react-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <>
+            <h1 className=' text-4xl text-center font-bold mt-8'>Password generator </h1>
+        </>
+    );
+}
+
