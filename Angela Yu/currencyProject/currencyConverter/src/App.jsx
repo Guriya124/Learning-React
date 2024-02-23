15e2ce919883c2e67b503d7d3dd34e9134ac2838
Guriya124 <guriyakumarisgvu@gmail.com>
2024-02-23 18:06:01 +0530

    Create currency converter in react

---
+++ Angela Yu/currencyProject/currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-[#bada55]'>Hello</h1>
+    </>
+  )
+}
+
+export default App
